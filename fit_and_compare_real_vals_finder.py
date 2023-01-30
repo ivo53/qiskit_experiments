fe6c70f5997771ce1acdc95281dca0fda17929c1
Ivo <ivo.mihov@protonmail.com>
2023-01-30 17:44:50 +0200

    sine models fitted with rz conjecture

--- fit_and_compare_real_vals_finder.py
+++ fit_and_compare_real_vals_finder.py
@@ -9,7 +9,6 @@ from scipy.optimize import root
 from scipy.integrate import quad, quad_vec
 from scipy.misc import derivative
 from scipy.special import ellipk
-import mpmath as mp
 
 AREA_VALUES = {
     "half": 0.5 * np.pi,
@@ -25,7 +24,10 @@ RABI_FREQ = {
     "rz": 42874911.4203,
     "gauss": 25179780.7441,
     "demkov": 31739880.846,
-    "sech2": 35460561.388
+    "sech2": 35460561.388,
+    "sine": 23131885.3151,
+    "sine2": 25244940.9663,
+    "sine3": 26023370.9794
 }
 
 Tt = {
@@ -34,7 +36,10 @@ Tt = {
     "rz": (284 + 4/9) * 1e-9,
     "gauss": (398 + 2/9) * 1e-9,
     "demkov": (572 + 4/9) * 1e-9,
-    "sech2": (284 + 4/9) * 1e-9
+    "sech2": (284 + 4/9) * 1e-9,
+    "sine": (213 + 1/3) * 1e-9,
+    "sine2": (248 + 8/9) * 1e-9,
+    "sine3": (284 + 4/9) * 1e-9
 }
 
 SIGMA = {
@@ -44,7 +49,12 @@ SIGMA = {
     "sech2": (44 + 4/9) * 1e-9
 }
 
-ALPHA = 0.4494679707017059
+ALPHA = {
+    "sech2": 0.4494679707017059,
+    "sine": 0.4494679707017059,
+    "sine2": 0.4494679707017059,
+    "sine3": 0.4494679707017059,
+}
 
 times = {
     "gauss": "174431",
@@ -60,7 +70,7 @@ times = {
 
 date = "2022-06-16"
 area = "pi"
-pulse_type = "rabi"
+pulse_type = "sine"
 fit_func = pulse_type
 baseline_fit_func = "sinc2" if pulse_type in ["rabi", "constant"] else "lorentzian"
 
@@ -125,7 +135,55 @@ def sech_sq(x, q_freq, delta, eps):
         return f_(t) * g_(t)
     tau = quad(f_, -1e-4, 1e-4, epsabs=1e-13, epsrel=1e-5)[0]
     G = quad_vec(fg_, -1e-4, 1e-4, epsabs=1e-13, epsrel=1e-5)[0]
-    P2 = np.sin(0.5 * tau * np.sqrt(omega_0 ** 2 + ALPHA * D ** 2)) ** 2 * np.abs(G / tau) ** 2
+    P2 = np.sin(0.5 * tau * np.sqrt(omega_0 ** 2 + ALPHA["sech2"] * D ** 2)) ** 2 * np.abs(G / tau) ** 2
+    return post_process(P2, eps, delta)
+
+def sine(x, q_freq, delta, eps):
+    T = Tt["sine"]
+    sigma = T / np.pi
+    omega_0 = RABI_FREQ["sine"]
+    D = (x - q_freq) * 1e6
+    def f_(t):
+        return np.heaviside(t + T/2, 1) * np.heaviside(T/2 - t, 1) * np.cos(t / sigma) 
+    def g_(t):
+        return np.exp(1j * D * t)
+    def fg_(t):
+        return f_(t) * g_(t)
+    tau = quad(f_, -1e-5, 1e-5, epsabs=1e-13, epsrel=1e-5)[0]
+    G = quad_vec(fg_, -1e-5, 1e-5, epsabs=1e-13, epsrel=1e-5)[0]
+    P2 = np.sin(0.5 * tau * np.sqrt(omega_0 ** 2 + ALPHA["sine"] * D ** 2)) ** 2 * np.abs(G / tau) ** 2
+    return post_process(P2, eps, delta)
+
+def sine2(x, q_freq, delta, eps):
+    T = Tt["sine2"]
+    sigma = T / np.pi
+    omega_0 = RABI_FREQ["sine2"]
+    D = (x - q_freq) * 1e6
+    def f_(t):
+        return np.heaviside(t + T/2, 1) * np.heaviside(T/2 - t, 1) * np.cos((t) / sigma) ** 2 
+    def g_(t):
+        return np.exp(1j * D * t)
+    def fg_(t):
+        return f_(t) * g_(t)
+    tau = quad(f_, -1e-5, 1e-5, epsabs=1e-13, epsrel=1e-5)[0]
+    G = quad_vec(fg_, -1e-5, 1e-5, epsabs=1e-13, epsrel=1e-5)[0]
+    P2 = np.sin(0.5 * tau * np.sqrt(omega_0 ** 2 + ALPHA["sine2"] * D ** 2)) ** 2 * np.abs(G / tau) ** 2
+    return post_process(P2, eps, delta)
+
+def sine3(x, q_freq, delta, eps):
+    T = Tt["sine3"]
+    sigma = T / np.pi
+    omega_0 = RABI_FREQ["sine3"]
+    D = (x - q_freq) * 1e6
+    def f_(t):
+        return np.heaviside(t + T/2, 1) * np.heaviside(T/2 - t, 1) * np.cos((t) / sigma) ** 3
+    def g_(t):
+        return np.exp(1j * D * t)
+    def fg_(t):
+        return f_(t) * g_(t)
+    tau = quad(f_, -1e-5, 1e-5, epsabs=1e-13, epsrel=1e-8)[0]
+    G = quad_vec(fg_, -1e-5, 1e-5, epsabs=1e-13, epsrel=1e-8)[0]
+    P2 = np.sin(0.5 * tau * np.sqrt(omega_0 ** 2 + ALPHA["sine3"] * D ** 2)) ** 2 * np.abs(G / tau) ** 2
     return post_process(P2, eps, delta)
 
 def gauss_sech2(x, q_freq, delta, eps):
@@ -193,7 +251,10 @@ FIT_FUNCTIONS = {
     "rz": rz,
     "demkov": demkov,
     "sech2": sech_sq,
-    "sinc2": sinc2
+    "sinc2": sinc2,
+    "sine": sine,
+    "sine2": sine2,
+    "sine3": sine3
 }
 file_dir = os.path.dirname(__file__)
 data_folder = os.path.join(file_dir, "data", "armonk", "calibration", date)
@@ -206,8 +267,20 @@ for d in data_files:
 
 pulse_type = d.split("_")[1]
 pulse_type = "rabi" if pulse_type in ["sq", "constant"] else pulse_type
-df = pd.read_csv(os.path.join(data_folder, csv_file))
+pulse_type = "rz" if pulse_type in ["sech", "rosenzener"] else pulse_type
 
+model_name_dict = {
+    "rabi": ["Rabi", "Sinc$^2$"], 
+    "rz": ["Rosen-Zener", "Lorentzian"], 
+    "gauss": ["Gaussian", "Lorentzian"], 
+    "demkov": ["Demkov", "Lorentzian"], 
+    "sech2": ["Sech$^2$", "Lorentzian"],
+    "sine": ["Sine", "Lorentzian"],
+    "sine2": ["Sine$^2$", "Lorentzian"],
+    "sine3": ["Sine$^3$", "Lorentzian"]
+}
+
+df = pd.read_csv(os.path.join(data_folder, csv_file))
 freq = df["frequency_ghz"].to_numpy() * 1e3
 vals = df["transition_probability"].to_numpy()
 detuning = 2 * np.pi * (freq - center_freq)
@@ -289,9 +362,17 @@ baseline_similarity_idx, baseline_y_fit, \
 # print(baseline_fit_params)
 # print(err)
 # print(baseline_err)
-print(fit_params[0], "+-", err[0])
-print(baseline_fit_params[-2], "+-", baseline_err[-2])
-print("deviation:", fit_params[0] - baseline_fit_params[-2], "+-", np.sqrt(err[0] ** 2 + baseline_err[-2] ** 2))
+
+print(model_name_dict[pulse_type][0])
+print("Model SI:", similarity_idx)
+print("Baseline SI:", baseline_similarity_idx)
+q_freq_model = fit_params[0] / (2 * np.pi)
+q_freq_bl = baseline_fit_params[-2] / (2 * np.pi)
+q_freq_err_model = err[0] / (2 * np.pi)
+q_freq_err_bl = baseline_err[-2] / (2 * np.pi)
+print(q_freq_model, "+-", q_freq_err_model)
+print(q_freq_bl, "+-", q_freq_err_bl)
+print("deviation:", q_freq_model - q_freq_bl, "+-", np.sqrt(q_freq_err_model ** 2 + q_freq_err_bl ** 2))
 # for o in np.linspace(5e5, 1e9, 500):
 #     fit_, baseline_ = fit_once(
 #         detuning, vals, fit_func, 0, 1, o,
@@ -300,34 +381,33 @@ print("deviation:", fit_params[0] - baseline_fit_params[-2], "+-", np.sqrt(err[0
 #     similarity_idx_, y, prms = fit_
 #     print("o =", o, "sim =", similarity_idx_, "rabi =", prms[-1])
 
+scaled_ef = extended_freq / (2 * np.pi)
+scaled_det = detuning / (2 * np.pi)
+
 fig = plt.figure(constrained_layout=True, figsize=(10,7))
 gs = fig.add_gridspec(7, 1)
 ax0 = fig.add_subplot(gs[:5, :])
-ax0.scatter(detuning, vals, color='black', marker="P")
-ax0.plot(extended_freq, baseline_extended_y_fit, color='blue')
-ax0.plot(extended_freq, extended_y_fit, color='red')
-ax0.set_xlim(extended_freq[0], -extended_freq[0])
-model_name_dict = {
-    "rabi": ["Rabi", "Sinc$^2$"], 
-    "rz": ["Rosen-Zener", "Lorentzian"], 
-    "gauss": ["Gaussian", "Lorentzian"], 
-    "demkov": ["Demkov", "Lorentzian"], 
-    "sech2": ["Sech$^2$", "Lorentzian"]
-}
+ax0.scatter(scaled_det, vals, color='black', marker="P")
+ax0.plot(scaled_ef, baseline_extended_y_fit, color='blue')
+ax0.plot(scaled_ef, extended_y_fit, color='red')
+ax0.set_xlim(scaled_ef[0], -scaled_ef[0])
 ax0.legend([
     "Experimental data", 
     f"{model_name_dict[pulse_type][1]} fit", 
     f"{model_name_dict[pulse_type][0]} model analytical fit"
 ])
-# major_xticks = np.round(np.arange(extended_freq[0], -extended_freq[0] + 1e-1, 5*2*np.pi),1)
-x_limit = np.floor(np.abs(extended_freq[0]) / 5) * 5
-x_interval = np.round(x_limit / 5) if pulse_type == "rabi" else np.round(x_limit / 6)
-x_small_interval = np.round(x_interval / 3) if pulse_type == "rabi" else np.round(x_limit / 30)
-# print(x_limit)
-major_xticks = np.round(np.arange(-x_limit, x_limit + 1e-3, x_interval),0)
+
+major_interval = 2.5 if pulse_type=="rabi" else 5.
+minor_interval = 0.5 if pulse_type=="rabi" else 1.
+major_xticks = np.round(np.arange(scaled_ef[0], -scaled_ef[0] + 1e-1, major_interval),1)
+# x_limit = np.floor(np.abs(extended_freq[0]) / 5) * 5
+# x_interval = np.round(x_limit / 5) if pulse_type == "rabi" else np.round(x_limit / 6)
+# x_small_interval = np.round(x_interval / 3) if pulse_type == "rabi" else np.round(x_limit / 30)
+# # print(x_limit)
+# major_xticks = np.round(np.arange(-x_limit, x_limit + 1e-3, x_interval),0)
 major_xticks[major_xticks>-0.01] = np.abs(major_xticks[major_xticks>-0.01])
-# minor_xticks = np.round(np.arange(extended_freq[0], -extended_freq[0] + 1e-1, 2*np.pi),1)
-minor_xticks = np.round(np.arange(-x_limit, x_limit + 1e-3, x_small_interval),0)
+minor_xticks = np.round(np.arange(scaled_ef[0], -scaled_ef[0] + 1e-1, minor_interval),1)
+# minor_xticks = np.round(np.arange(-x_limit, x_limit + 1e-3, x_small_interval),0)
 major_yticks = np.arange(0, 1.01, 0.2).round(1)
 minor_yticks = np.arange(0, 1.01, 0.1).round(1)
 
@@ -365,7 +445,7 @@ y_ticks_res = np.arange(-limit_num, limit_num + 1e-3, tick_interval).round(2)
 err = 1 / np.sqrt(2048)
 ax1 = fig.add_subplot(gs[5:6, :])
 ax1.set_ylim(-limit_num, limit_num)
-ax1.set_xlim(extended_freq[0], -extended_freq[0])
+ax1.set_xlim(scaled_ef[0], -scaled_ef[0])
 
 ax1.set_xticks(major_xticks)
 ax1.set_xticklabels([])
@@ -378,26 +458,26 @@ if limit_num > 0.1:
     ax1.grid(which='major', alpha=0.6)
 else:
     ax1.grid()
-ax1.errorbar(detuning, y_fit - vals, yerr=err * np.ones(detuning.shape), fmt="+", color="r")
+ax1.errorbar(scaled_det, y_fit - vals, yerr=err * np.ones(scaled_det.shape), fmt="+", color="r")
 # print(major_xticks)
 ax2 = fig.add_subplot(gs[6:, :], sharey=ax1)
-ax2.set_xlim(extended_freq[0], -extended_freq[0])
+ax2.set_xlim(scaled_ef[0], -scaled_ef[0])
 ax2.set_xticks(major_xticks)
 ax2.set_xticklabels(np.round(major_xticks, 1), fontsize=16)
 ax2.set_xticks(minor_xticks, minor="True")
 ax2.set_yticklabels(y_ticks_res, fontsize=13)
-ax2.errorbar(detuning, baseline_y_fit - vals, yerr=err * np.ones(detuning.shape), fmt="+", color="b")
+ax2.errorbar(scaled_det, baseline_y_fit - vals, yerr=err * np.ones(scaled_det.shape), fmt="+", color="b")
 if limit_num > 0.1:
     ax2.set_yticks(y_ticks_res_minor, minor=True)
     ax2.grid(which='minor', alpha=0.3)
     ax2.grid(which='major', alpha=0.6)
 else:
     ax2.grid()
-plt.xlabel("Detuning [$\\times 10^6$ rad/s]", fontsize=20)
+plt.xlabel("Detuning [MHz]", fontsize=20)
 
 # fig2_name = date.strftime("%H%M%S") + f"_{pulse_type}_area_{area}_frequency_sweep_fitted.png" if pulse_type not in lorentz_pulses else \
 #     date.strftime("%H%M%S") + f"_{pulse_type}_cutoff_{cutoff}_{ctrl_param}_{c_p}_area_{area}_frequency_sweep_fitted.png"
-save_dir = "C:/Users/Ivo/Documents/PhD Documents/Pulse Shapes"
+save_dir = "C:/Users/Ivo/Documents/PhD Documents/Sine Pulses"
 date = datetime.now()
 fig_name = pulse_type + "_" + date.strftime("%Y%m%d") + ".pdf"
 plt.savefig(os.path.join(save_dir, fig_name), format="pdf")
---
+++ fit_numerical_sech2.py
@@ -0,0 +1,188 @@
+# detlist=[-93000000, -92000000, -91000000, -90000000, -89000000, -88000000, \
+# -87000000, -86000000, -85000000, -84000000, -83000000, -82000000, \
+# -81000000, -80000000, -79000000, -78000000, -77000000, -76000000, \
+# -75000000, -74000000, -73000000, -72000000, -71000000, -70000000, \
+# -69000000, -68000000, -67000000, -66000000, -65000000, -64000000, \
+# -63000000, -62000000, -61000000, -60000000, -59000000, -58000000, \
+# -57000000, -56000000, -55000000, -54000000, -53000000, -52000000, \
+# -51000000, -50000000, -49000000, -48000000, -47000000, -46000000, \
+# -45000000, -44000000, -43000000, -42000000, -41000000, -40000000, \
+# -39000000, -38000000, -37000000, -36000000, -35000000, -34000000, \
+# -33000000, -32000000, -31000000, -30000000, -29000000, -28000000, \
+# -27000000, -26000000, -25000000, -24000000, -23000000, -22000000, \
+# -21000000, -20000000, -19000000, -18000000, -17000000, -16000000, \
+# -15000000, -14000000, -13000000, -12000000, -11000000, -10000000, \
+# -9000000, -8000000, -7000000, -6000000, -5000000, -4000000, -3000000, \
+# -2000000, -1000000, 0, 1000000, 2000000, 3000000, 4000000, 5000000, \
+# 6000000, 7000000, 8000000, 9000000, 10000000, 11000000, 12000000, \
+# 13000000, 14000000, 15000000, 16000000, 17000000, 18000000, 19000000, \
+# 20000000, 21000000, 22000000, 23000000, 24000000, 25000000, 26000000, \
+# 27000000, 28000000, 29000000, 30000000, 31000000, 32000000, 33000000, \
+# 34000000, 35000000, 36000000, 37000000, 38000000, 39000000, 40000000, \
+# 41000000, 42000000, 43000000, 44000000, 45000000, 46000000, 47000000, \
+# 48000000, 49000000, 50000000, 51000000, 52000000, 53000000, 54000000, \
+# 55000000, 56000000, 57000000, 58000000, 59000000, 60000000, 61000000, \
+# 62000000, 63000000, 64000000, 65000000, 66000000, 67000000, 68000000, \
+# 69000000, 70000000, 71000000, 72000000, 73000000, 74000000, 75000000, \
+# 76000000, 77000000, 78000000, 79000000, 80000000, 81000000, 82000000, \
+# 83000000, 84000000, 85000000, 86000000, 87000000, 88000000, 89000000, \
+# 90000000, 91000000, 92000000, 93000000]
+# sech2list=[0.000218194, 0.000238388, 0.000260939, 0.000286386, 0.000315369, \
+# 0.000348646, 0.000387089, 0.000431727, 0.000483747, 0.000544516, \
+# 0.000615608, 0.000698816, 0.000796178, 0.000909991, 0.00104284, \
+# 0.00119758, 0.00137742, 0.00158584, 0.00182668, 0.00210409, \
+# 0.00242258, 0.00278699, 0.00320251, 0.00367469, 0.00420944, \
+# 0.00481306, 0.00549227, 0.00625422, 0.00710654, 0.00805743, \
+# 0.0091157, 0.0102909, 0.0115932, 0.013034, 0.0146256, 0.0163814, \
+# 0.0183164, 0.0204469, 0.0227912, 0.0253693, 0.0282033, 0.0313179, \
+# 0.0347401, 0.0384996, 0.042629, 0.0471639, 0.052143, 0.0576081, \
+# 0.0636043, 0.0701799, 0.0773861, 0.0852768, 0.0939088, 0.103341, \
+# 0.113633, 0.124846, 0.137042, 0.150281, 0.164622, 0.18012, 0.196826, \
+# 0.214786, 0.234038, 0.254609, 0.276519, 0.299772, 0.32436, 0.350258, \
+# 0.377426, 0.405804, 0.435311, 0.465849, 0.497297, 0.529515, 0.562339, \
+# 0.595588, 0.629061, 0.662542, 0.695796, 0.728579, 0.760636, 0.791704, \
+# 0.82152, 0.849819, 0.876344, 0.900844, 0.923084, 0.942842, 0.959921, \
+# 0.974144, 0.985366, 0.993467, 0.998362, 1., 0.998362, 0.993467, \
+# 0.985366, 0.974144, 0.959921, 0.942842, 0.923084, 0.900844, 0.876344, \
+# 0.849819, 0.82152, 0.791704, 0.760636, 0.728579, 0.695796, 0.662542, \
+# 0.629061, 0.595588, 0.562339, 0.529515, 0.497297, 0.465849, 0.435311, \
+# 0.405804, 0.377426, 0.350258, 0.32436, 0.299772, 0.276519, 0.254609, \
+# 0.234038, 0.214786, 0.196826, 0.18012, 0.164622, 0.150281, 0.137042, \
+# 0.124846, 0.113633, 0.103341, 0.0939088, 0.0852768, 0.0773861, \
+# 0.0701799, 0.0636043, 0.0576081, 0.052143, 0.0471639, 0.042629, \
+# 0.0384996, 0.0347401, 0.0313179, 0.0282033, 0.0253693, 0.0227912, \
+# 0.0204469, 0.0183164, 0.0163814, 0.0146256, 0.013034, 0.0115932, \
+# 0.0102909, 0.0091157, 0.00805743, 0.00710654, 0.00625422, 0.00549227, \
+# 0.00481306, 0.00420944, 0.00367469, 0.00320251, 0.00278699, \
+# 0.00242258, 0.00210409, 0.00182668, 0.00158584, 0.00137742, \
+# 0.00119758, 0.00104284, 0.000909991, 0.000796178, 0.000698816, \
+# 0.000615608, 0.000544516, 0.000483747, 0.000431727, 0.000387089, \
+# 0.000348646, 0.000315369, 0.000286386, 0.000260939, 0.000238388, \
+# 0.000218194]
+# detlist = np.array(detlist) / 1e6
+# sech2_extended_freq=np.linspace(detlist[0], detlist[-1], 5000)
+def fit_function(x_values, y_values, function, init_params, lower, higher):
+    fitparams, conv = curve_fit(function, x_values, y_values, init_params, maxfev=1e6, bounds=(lower, higher))
+    y_fit = function(x_values, *fitparams)
+    perr = np.sqrt(np.diag(conv))
+    return fitparams, y_fit, perr
+
+def post_process(P2, eps, delta):
+    # eps = eps0 + 1/2 * (1 - eps1)
+    # delta = 1/2 * (1 - eps1) * e^(-gamma * t)
+    return eps + delta * (2 * P2 - 1)
+
+def with_dephasing(P2, egamma):
+    return P2 * egamma - egamma / 2 + 1 / 2
+
+def lorentzian(x, s, A, q_freq, c):
+    return A / (((x - q_freq) / s) ** 2 + 1) + c
+
+def sech_sq(x, q_freq, delta, eps):
+    T = Tt["sech2"]
+    sigma = SIGMA["sech2"]
+    omega_0 = RABI_FREQ["sech2"]
+    D = (x - q_freq) * 1e6
+    def f_(t):
+        return 1 / np.cosh((t) / sigma) ** 2 
+    def g_(t):
+        return np.exp(1j * D * t)
+    def fg_(t):
+        return f_(t) * g_(t)
+    tau = quad(f_, -1e-4, 1e-4, epsabs=1e-13, epsrel=1e-5)[0]
+    G = quad_vec(fg_, -1e-4, 1e-4, epsabs=1e-13, epsrel=1e-5)[0]
+    P2 = np.sin(0.5 * tau * np.sqrt(omega_0 ** 2 + ALPHA * D ** 2)) ** 2 * np.abs(G / tau) ** 2
+    return post_process(P2, eps, delta)
+def fit_once(
+    detuning, vals, fit_func,
+    args, args_min, args_max,
+    ef=None
+):
+    # initial = [0, 0.4, 0.4, 0.4] if fit_func in ["sech2"] else [0.1, 0, 0]
+    # initial_min = [-3, 0.3, 0.3, 0] if fit_func in ["sech2"] else [-3, 0, 0]
+    # initial_max = [3, 0.5, 0.6, 1] if fit_func in ["sech2"] else [3, 0.5, 0.6]
+    initial = [0.1, 0, 0]
+    initial_min = [-3, 0, 0]
+    initial_max = [3, 0.5, 0.6]
+    fit_params, y_fit, err = fit_function(
+        detuning,#[int(len(detuning) / 4):int(3 * len(detuning) / 4)],
+        vals,#[int(len(detuning) / 4):int(3 * len(detuning) / 4)], 
+        FIT_FUNCTIONS[fit_func],
+        initial, initial_min, initial_max
+    )
+    y_fit = FIT_FUNCTIONS[fit_func](detuning, *fit_params)
+    ##
+    ##
+    baseline_fit_params, baseline_y_fit, baseline_err = fit_function(
+        detuning,
+        vals, 
+        FIT_FUNCTIONS[baseline_fit_func],
+        [2, 0, 1, 0], # initial parameters for curve_fit
+        [0, -10, 0, 0],
+        [100, 10, 100, 0.5]
+
+        # [1, 0, 1], # initial parameters for curve_fit
+        # [0, -10, 0],
+        # [10, 10, 1]
+    )
+    ##
+    # print(fit_params, "\n", baseline_fit_params)
+    if ef is not None: 
+        ef = sech2_extended_freq
+    else:
+        ef = extended_freq
+    extended_y_fit = FIT_FUNCTIONS[fit_func](ef, *fit_params)
+    baseline_extended_y_fit = FIT_FUNCTIONS[baseline_fit_func](ef, *baseline_fit_params)
+
+    similarity_idx = np.sum(np.abs(y_fit - vals))
+    baseline_similarity_idx = np.sum(np.abs(baseline_y_fit - vals))
+
+    overfitting_idx = np.mean(np.abs(np.diff(extended_y_fit)))
+    baseline_overfitting_idx = np.mean(np.abs(np.diff(baseline_extended_y_fit)))
+    overfitting = overfitting_idx > 0.1
+    baseline_overfitting = baseline_overfitting_idx > 0.1
+    # print(overfitting_idx, baseline_overfitting_idx)
+    if overfitting:
+        print("Strong overfitting present.")
+        exit(1)
+    return (similarity_idx, 
+            y_fit, 
+            extended_y_fit, 
+            fit_params,
+            err), \
+           (baseline_similarity_idx, 
+            baseline_y_fit, 
+            baseline_extended_y_fit, 
+            baseline_fit_params,
+            baseline_err)
+
+fit, baseline = fit_once(
+    detuning, vals, fit_func,
+    args=[0, 1, 1e6], 
+    args_min=[-3, .99, 1e4],
+    args_max=[3, 1., 1e8]
+)
+
+
+# sech2list = np.array(sech2list)
+# sech2_fit, baseline_fit = fit_once(
+#     detlist, sech2list, "sech2",
+#     args=[0, 1, 1e6], 
+#     args_min=[-3, .99, 1e4],
+#     args_max=[3, 1., 1e8],
+#     ef=1
+# )
+# sech2_similarity_idx, sech2_y_fit, sech2_extended_y_fit, sech2_fit_params, sech2_err = sech2_fit
+# bs_similarity_idx, bs_y_fit, bs_extended_y_fit, bs_fit_params, bs_err = baseline_fit
+# print(sech2_fit_params[-1], sech2_err[-1])
+# print(sech2_similarity_idx, bs_similarity_idx)
+
+# fig = plt.figure(constrained_layout=True, figsize=(10,7))
+# gs = fig.add_gridspec(7, 1)
+# ax0 = fig.add_subplot(gs[:5, :])
+# ax0.scatter(detlist, sech2list, color='black', marker="P")
+# ax0.plot(sech2_extended_freq, bs_extended_y_fit, color='blue')
+# ax0.plot(sech2_extended_freq, sech2_extended_y_fit, color='red')
+# ax0.set_xlim(sech2_extended_freq[0], -sech2_extended_freq[0])
+# plt.show()
+# exit(0)
