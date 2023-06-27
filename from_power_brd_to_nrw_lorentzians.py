f4de5e3c3154db08873d7b1d3537e1b38b76d29c
Ivo <ivo.mihov@protonmail.com>
2023-06-27 16:35:47 +0300

    27/06 evening uni

---
+++ from_power_brd_to_nrw_lorentzians.py
@@ -0,0 +1,49 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Define the Lorentzian function
+def lorentzian(x, x0, sigma):
+    return 1 / (((x - x0) / sigma) ** 2 + 1)
+
+# Define the truncated Lorentzian function
+def truncated_lorentzian(x, x0, sigma, duration):
+    return np.where(np.abs(x - x0) <= duration/2, lorentzian(x, x0, sigma), 0)
+
+# Generate x values
+x = np.linspace(-320, 320, 1000)
+
+# Define the parameters for each subplot
+parameters = [
+    (0, 21.33, 42.666666666),
+    (0, 21.33, 85.3333333333),
+    (0, 21.33, 101.566982095),
+    (0, 21.33, 149.84040893),
+    (0, 21.33, 185.979688258),
+    (0, 21.33, 242.612936664),
+    (0, 21.33, 298.666666667),
+    (0, 21.33, 424.527973164),
+    (0, 21.33, 601.8874018)
+]
+
+# Create the grid of plots
+fig, axes = plt.subplots(3, 3, figsize=(12, 9), sharex='col', sharey='row')
+
+# Iterate over each subplot
+for ax, params in zip(axes.flat, parameters):
+    x0, sigma, duration = params
+    y = truncated_lorentzian(x, x0, sigma, duration)
+    ax.plot(x, y, color='black', linewidth=0.6)
+    ax.axvline(-duration/2, color='r', linestyle='--', linewidth=0.6)
+    ax.axvline(duration/2, color='r', linestyle='--', linewidth=0.6)
+    ax.set_title(f"duration={np.round(duration, 2)} ns")
+for ax in axes[-1]:
+    ax.set_xlabel('Time [ns]')
+for ax in axes[:, 0]:
+    ax.set_ylabel('Rabi Frequency [normalised]')
+
+# Adjust the spacing between subplots
+plt.tight_layout()
+
+plt.savefig(f"C:/Users/Ivo/Downloads/lorentzian_grid_{np.random.randint(1000)}.pdf", format="PDF")
+# Display the grid of plots
+plt.show()
