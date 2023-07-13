5e4e617e21cce25e2b4ecf73344341fc76c217c8
VA <vasil.avramov@phys.uni-sofia.bg>
2023-07-13 08:24:47 +0300

    13/07 morning home

--- numerical_solutions.py
+++ numerical_solutions.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import qutip
 from scipy.integrate import quad
 from scipy.integrate import solve_ivp
 
@@ -12,28 +13,39 @@ def omega_dot(t, omega_0, sigma, lamb=1):
 sigma = 24.8888888889e-9
 T = 704e-9
 omega_0 = np.pi / quad(lambda t: omega(t, 1, sigma), -T/2, T/2)[0]
+# print(omega_0)
+# def c2(t, y, d, omega_0, sigma, lamb):
+#     # Define the second-order differential equation
+#     dc2_2dt_2 = (omega_dot(t, omega_0, sigma, lamb=lamb) /\
+#                  omega(t, omega_0, sigma, lamb=lamb) - 1j * d) * y[1] + \
+#                  (1j * omega_dot(t, omega_0, sigma, lamb=lamb) * \
+#                  d / omega(t, omega_0, sigma, lamb=lamb) - \
+#                  omega(t, omega_0, sigma, lamb=lamb) ** 2 / 4) * y[0]
+#     return y[2], np.array(dc2_2dt_2)
 
-def c2(y, t, delta=0):
-    # Define the second-order differential equation
-    dc2_2dt_2 = (omega_dot(t, omega_0, sigma) / omega(t, omega_0, sigma) - 1j * delta) * y[1] + (1j * omega_dot(t, omega_0, sigma) * delta / omega(t, omega_0, sigma) - omega(t, omega_0, sigma) ** 2 / 4) * y[0]
-    return dc2_2dt_2
-
-
+ 
 
 # Define the initial conditions
 y0 = [0, 1]  # Example initial conditions: y(0) = 0, y'(0) = 1
+l = 1
 
-# Define the time points at which you want to evaluate the solution
-t = np.linspace(-10, 10, 100)  # Example time span: 0 to 10 with 100 points
 det = np.linspace(-100e6, 100e6, 100)
 # Solve the second-order differential equation
-for d in det:
-    y = solve_ivp(lambda y, t: c2(y, t, d), (-10, 10), [0])
+c2_values = np.empty((len(det)), dtype=np.ndarray)
+c2dt_values = np.empty((len(det)), dtype=np.ndarray)
+c2dt2_values = np.empty((len(det)), dtype=np.ndarray)
+t_values = np.empty((len(det)), dtype='float')
+for i, d in enumerate(det):
+    y = solve_ivp(lambda t, y: c2(t, y, d, omega_0, sigma, l), (-T/2, T/2), [0, 0, 0], max_step=1e-8)
+    c2_values[i] = y.y[0][-1]
+    c2dt_values[i] = y.y[1][-1]
+    # c2dt2_values[i] = y.y[2][-1]
+    t_values[i] = y.t[-1]
 
+print(c2_values)
 # Plot the solution
-plt.plot(t, y[:, 0], 'r', label='y(t)')  # Plotting y(t)
-plt.plot(t, y[:, 1], 'b', label="y'(t)")  # Plotting y'(t)
+plt.plot(det, np.abs(c2_values) ** 2, 'r', label='y(t)')  # Plotting y(t)
 plt.xlabel('Time')
-plt.ylabel('Solution')
+plt.ylabel('Transition probability')
 plt.legend()
 plt.show()
