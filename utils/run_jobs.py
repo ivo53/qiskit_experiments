f568f7924c44537091657383d8e6f62acb15348c
Ivo <ivo.mihov@protonmail.com>
2023-12-05 16:54:04 +0200

    autocommit 14/08 uni
    Former-commit-id: 5dd8410276a2d063de7e161f96e28eb4c6ebf98a

--- utils/run_jobs.py
+++ utils/run_jobs.py
@@ -1,3 +1,6 @@
+import time
+from qiskit.providers.jobstatus import JobStatus
+
 SIZE_LIMIT = 250000
 CIRC_LIMIT = 100
 def run_jobs(circs, backend, duration, num_shots_per_exp=1024):
@@ -46,11 +49,30 @@ def run_jobs(circs, backend, duration, num_shots_per_exp=1024):
         # print("len_parts", len(parts))
         # print("option 2")
         # return
-        for circs_part in parts:
+        num_queued = 0
+        for idx, circs_part in enumerate(parts):
             current_job = backend.run(
                 circs_part,
                 shots=num_shots_per_exp
             )
+            num_queued += 1
+            # wait if more than three jobs are queued
+            if num_queued > 3:
+                try:
+                    first_job_status = jobs[-2].status() 
+                    while first_job_status is JobStatus.QUEUED:
+                        time.sleep(30)
+                    while current_job.status() is JobStatus.CANCELLED:
+                        current_job = backend.run(
+                            circs_part,
+                            shots=num_shots_per_exp
+                        )
+                        while current_job.status() is JobStatus.INITIALIZING or \
+                              current_job.status() is JobStatus.VALIDATING:
+                            time.sleep(30)
+                    num_queued -= 1
+                except IBMJobApiError as ex:
+                    print("Error encountered: {}".format(ex))
             jobs.append(current_job)
             job_ids.append(current_job.job_id())
         
