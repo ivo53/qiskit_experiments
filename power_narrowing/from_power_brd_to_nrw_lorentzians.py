5d0716c461191eb8a1e3d4f60160d962e331ead3
ivo <ivo.mihov@protonmail.com>
2024-03-22 02:42:10 +0200

    22/03 evening home

--- power_narrowing/from_power_brd_to_nrw_lorentzians.py
+++ power_narrowing/from_power_brd_to_nrw_lorentzians.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib; matplotlib.use('Agg')
 
 # Define the Lorentzian function
 def lorentzian(x, x0, sigma):
@@ -32,28 +33,31 @@ fig, axes = plt.subplots(3, 3, figsize=(12, 9), sharex='col', sharey='row')
 for ax, params in zip(axes.flat, parameters):
     x0, sigma, duration = params
     y = truncated_lorentzian(x, x0, sigma, duration)
-    ax.plot(x, y, color='black', linewidth=0.6)
-    ax.axvline(-duration/2, color='r', linestyle='--', linewidth=0.6)
-    ax.axvline(duration/2, color='r', linestyle='--', linewidth=0.6)
+    ax.plot(x, y, color='black', linewidth=1.5)
+    ax.axvline(-duration/2, color='r', linestyle='--', linewidth=1.5)
+    ax.axvline(duration/2, color='r', linestyle='--', linewidth=1.5)
     # ax.set_title(f"duration={np.round(duration, 2)} ns")
-    param_box_text = f'duration={np.round(duration, 2)} ns'
+    param_box_text = f'T={int(np.round(duration, 0))} ns'
     ax.text(
         0.58,
         0.95,
         param_box_text,
-        fontsize=8,
+        fontsize=18,
         transform=ax.transAxes,
         verticalalignment='top',
         bbox=dict(facecolor='white', alpha=0.8)
     )
+    ax.tick_params(axis='x', labelsize=16)
+    ax.tick_params(axis='y', labelsize=16)
+
 for ax in axes[-1]:
-    ax.set_xlabel('Time [ns]')
+    ax.set_xlabel('Time [ns]', fontsize=18)
 for ax in axes[:, 0]:
-    ax.set_ylabel('Rabi Frequency [normalised]')
+    ax.set_ylabel('Rabi Freq. [au]', fontsize=18)
 
 # Adjust the spacing between subplots
 plt.tight_layout()
 
 plt.savefig(f"C:/Users/Ivo/Downloads/lorentzian_grid_{np.random.randint(1000)}.pdf", format="PDF")
 # Display the grid of plots
-plt.show()
+# plt.show()
