00c6907822cb6d9fb7c1ca49a82d7d815bdeb9c2
Ivo <ivo.mihov@protonmail.com>
2024-03-22 18:50:35 +0200

    22/03 evening uni
    Former-commit-id: af6c28f4541e59429b0aa860124797bb3bfdb853

--- optimised_pulses.py
+++ optimised_pulses.py
@@ -21,7 +21,7 @@ def adiab_cond(t_, args, func):
     theta1_dot = omega_dot * D / (omega ** 2 + D ** 2)
     eps1 = (omega ** 2 + D ** 2) ** (1/2)
     adiabaticity = np.abs(theta1_dot / eps1)
-    #Now calculate theta2 and thus the superadiabaticity
+    # Now calculate theta2 and thus the superadiabaticity
     eps1_dot = np.diff(eps1)
     eps1_dot = np.append(eps1_dot, eps1[-1] - D)
     theta1_ddot = np.diff(theta1_dot)
@@ -29,8 +29,12 @@ def adiab_cond(t_, args, func):
     theta2_dot = (eps1 * theta1_ddot - eps1_dot * theta1_dot) / (eps1 ** 2 + 4 * theta1_dot ** 2)
     eps2 = (eps1 ** 2 + 4 * theta1_dot ** 2) ** (1/2)
     superadiabaticity = np.abs(theta2_dot / eps2)
+    
     return np.amax(superadiabaticity)
 
+def find_nth_adiabaticity(t_, args, func, n=5):
+    omega = func(t_, args)
+    thetan = 0.5 * arctan
 T, D = 5, 0.1
 t_ = np.linspace(-2*T,2*T, 500)
 # Define bounds for optimization
@@ -42,12 +46,12 @@ bounds = 2 * np.array([(-1, 1), (-1, 1), (-1, 1),
 
 # Initial guess for optimization
 
-res, it = 1, 0
+res, it = 1, 1878
 # Perform optimization
-while res > 0.15:
+while res > 0.14:
     np.random.seed(it)
     x0 = np.random.random(13)
-    result = minimize(lambda args: adiab_cond(t_, args, generic_shape), x0, bounds=bounds, options={"maxiter": 100000})
+    result = minimize(lambda args: adiab_cond(t_, args, generic_shape), x0, options={"maxiter": 100000})
     res = result.fun
     it += 1
 
