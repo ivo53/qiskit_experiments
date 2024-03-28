c039804ff7bedcd904b5454933ac0ab74a0ac7a6
Ivo <ivo.mihov@protonmail.com>
2024-03-28 17:52:25 +0200

    28/03 evening uni

--- new_pulse_codes/composite_pulses_profile_clearing.py
+++ new_pulse_codes/composite_pulses_profile_clearing.py
@@ -21,26 +21,15 @@ from qiskit.circuit import Parameter, Gate
 from qiskit.pulse import Delay,Play
 # This Pulse module helps us build sampled pulses for common pulse shapes
 from qiskit.pulse import library as pulse_lib
-from qiskit.providers.ibmq.managed import IBMQJobManager
+# from qiskit.providers.ibmq.managed import IBMQJobManager
 from qiskit_ibm_provider import IBMProvider
 
 current_dir = os.path.dirname(__file__)
 package_path = os.path.abspath(os.path.split(current_dir)[0])
 sys.path.insert(0, package_path)
 
-import pulse_types as pt
-
-
-def make_all_dirs(path):
-    path = path.replace("\\", "/")
-    folders = path.split("/")
-    for i in range(2, len(folders) + 1):
-        folder = "/".join(folders[:i])
-        if not os.path.isdir(folder):
-            os.mkdir(folder)
-
-def get_closest_multiple_of_16(num):
-    return int(num + 8) - (int(num + 8) % 16)
+from utils.run_jobs import run_jobs
+import common.pulse_types as pt
 
 pulse_dict = {
     "gauss": [pt.Gaussian, pt.LiftedGaussian],
@@ -57,39 +46,202 @@ pulse_dict = {
     "sin5": [pt.Sine5, pt.Sine5],
     "demkov": [pt.Demkov, pt.LiftedDemkov],
 }
-pulse_sequence_dict = {
-    3: np.array([0, 1, 0]) * np.pi / 2,
-    5: np.array([0, 5, 2, 5, 0]) * np.pi / 6,
-    7: np.array([0, 11, 10, 17, 10, 11, 0]) * np.pi / 12,
-    9: np.array([0, 0.366, 0.638, 0.435, 1.697, 0.435, 0.638, 0.366, 0]) * np.pi,
-    11: np.array([0, 11, 10, 23, 1, 19, 1, 23, 10, 11, 0]) * np.pi / 12,
-    13: np.array([0, 9, 42, 11, 8, 37, 2, 37, 8, 11, 42, 9, 0]) * np.pi / 24,
-    25: np.array([0, 5, 2, 5, 0, 11, 4, 1, 4, 11, 2, 7, 4, 7, 2, 11, 4, 1, 4, 11,\
-                    0, 5, 2, 5, 0]) * np.pi / 6,
+
+COMP_PARAMS = {
+    3: {"alpha": 0.6399, "phases": [1.8442, 1.0587]},
+    5: {"alpha": 0.45, "phases": [1.9494, 0.5106, 1.31]},
+    7: {"alpha": 0.2769, "phases": [1.6803, 0.2724, 0.8255, 1.6624]},
+    9: {"alpha": 0.2947, "phases": [0.2711, 1.1069, 1.5283, 0.1283, 0.9884]},
+    11: {"alpha": 0.2985, "phases": [1.7377, 0.1651, 0.9147, 0.1510, 0.9331, 1.6415]},
+    13: {"alpha": 0.5065, "phases": [0.0065, 1.7755, 0.7155, 0.5188, 0.2662, 1.2251, 1.3189]},
+    15: {"alpha": 0.3213, "phases": [1.2316, 0.9204, 0.2043, 1.9199, 0.8910, 0.7381, 1.9612, 1.3649]},
 }
+
+def get_calib_params(
+    backend, pulse_type, 
+    sigma, duration,
+    remove_bg
+):
+    file_dir = os.path.dirname(__file__)
+    file_dir = os.path.split(file_dir)[0]
+    calib_dir = os.path.join(file_dir, "calibrations", backend)
+    params_file = os.path.join(calib_dir, "actual_params.csv")
+    
+    if os.path.isfile(params_file):
+        param_df = pd.read_csv(params_file)
+    df = param_df[param_df.apply(
+            lambda row: row["pulse_type"] == pulse_type and \
+                row["duration"] == duration and \
+                row["sigma"] == sigma and \
+                row["rb"] == remove_bg, axis=1)]
+    # print(df)
+    idx = 0 
+    if df.shape[0] > 1:
+        idx = input("More than one identical calibrations found! "
+                    "Which do you want to use? ")
+    elif df.shape[0] < 1:
+        raise ValueError("No entry found!")
+    ser = df.iloc[idx]
+    l = ser.at["l"]
+    p = ser.at["p"]
+    x0 = ser.at["x0"]
+
+    return l, p, x0
+
+def initialize_backend(backend):
+    backend_full_name = "ibm_" + backend 
+        # if backend in ["perth", "lagos", "nairobi", "oslo"] \
+        #     else "ibmq_" + backend
+    GHz = 1.0e9 # Gigahertz
+    MHz = 1.0e6 # Megahertz
+    us = 1.0e-6 # Microseconds
+    ns = 1.0e-9 # Nanoseconds
+    qubit = 0
+    mem_slot = 0
+
+    drive_chan = pulse.DriveChannel(qubit)
+    # meas_chan = pulse.MeasureChannel(qubit)
+    # acq_chan = pulse.AcquireChannel(qubit)
+    
+    backend_name = backend
+    # provider = IBMQ.load_account()
+    backend = IBMProvider().get_backend(backend_full_name)
+    print(f"Using {backend_name} backend.")
+    backend_defaults = backend.defaults()
+    backend_config = backend.configuration()
+
+    center_frequency_Hz = backend_defaults.qubit_freq_est[qubit]# 4962284031.287086 Hz
+    num_qubits = backend_config.n_qubits
+
+    q_freq = [backend_defaults.qubit_freq_est[q] for q in range(num_qubits)]
+    dt = backend_config.dt
+
+    return backend, drive_chan, num_qubits, q_freq
+
+def fit_function(
+    x_values, 
+    y_values, 
+    function, 
+    init_params,
+    bounds
+):
+    fitparams, conv = curve_fit(
+        function, 
+        x_values, 
+        y_values, 
+        init_params, 
+        maxfev=100000, 
+        bounds=bounds
+    )
+    y_fit = function(x_values, *fitparams)
+    
+    return fitparams, y_fit
+
+def linear_func(x, a, b):
+    return a * x + b
+
+def run_check(
+    amp_span,
+    duration, sigma, pulse_type, remove_bg,
+    num_exp=10, N_max=100,
+    N_interval=2, max_exp_per_job=50,
+    num_shots=1024, backend="manila",
+    l=100, p=0.5, x0=0,
+    closest_amp=None
+):
+    backend, drive_chan, num_qubits, q_freq = initialize_backend(backend)
+    if closest_amp is None:
+        closest_amp = -np.log(1 - np.pi / l) / p + x0
+    amplitudes = np.linspace(
+        closest_amp - amp_span / 2,
+        closest_amp + amp_span / 2,
+        num_exp
+    )
+    Ns = np.arange(0, N_max + N_interval / 2, N_interval, dtype="int64")
+
+    def add_circ(amp, duration, sigma, freq, Ns, qubit=0):
+        amp = Parameter("amp")
+        phi = Parameter("phi")
+        with pulse.build(backend=backend, default_alignment='sequential', name="calibrate_area_with_N_pulses") as sched:
+            dur_dt = duration
+            pulse.set_frequency(freq, drive_chan)
+            if pulse_type == "sq" or "sin" in pulse_type:
+                pulse_played = pulse_dict[pulse_type][remove_bg](
+                    duration=dur_dt,
+                    amp=amp * np.exp(1j * phi * np.pi),
+                    name=pulse_type
+                )
+            elif pulse_type == "gauss":
+                pulse_played = pulse_dict[pulse_type][remove_bg](
+                    duration=dur_dt,
+                    amp=amp * np.exp(1j * phi * np.pi),
+                    name=pulse_type,
+                    sigma=sigma / np.sqrt(2)
+                )
+            elif pulse_type in ["lor", "lor2", "lor3"]:
+                pulse_played = pulse_dict[pulse_type][remove_bg](
+                    duration=dur_dt,
+                    amp=amp * np.exp(1j * phi * np.pi),
+                    name=pulse_type,
+                    sigma=sigma,
+                )
+            else:
+                pulse_played = pulse_dict[pulse_type][remove_bg](
+                    duration=dur_dt,
+                    amp=amp * np.exp(1j * phi * np.pi),
+                    name=pulse_type,
+                    sigma=sigma,
+                )
+            pulse.play(pulse_played, drive_chan)
+        base_circ = QuantumCircuit(num_qubits, len(Ns))
+        custom_gate = Gate("N_pulses", 1, [])
+        for _ in range(N):
+            base_circ.append(custom_gate, [qubit])
+        base_circ.measure(qubit, 0)
+        base_circ.add_calibration(custom_gate, (qubit,), sched, [])
+        return base_circ
+
+
+    circs = [add_circ(a, duration, sigma, q_freq[qubit], Ns) for a in amplitudes]
+
+    max_experiments_per_job = 100
+    num_shots = 1024
+
+    sweep_values, job_ids = run_jobs(circs, backend, duration *len(Ns)  , num_shots_per_exp=num_shots)
+
+    print(Ns, np.array(sweep_values).reshape(np.round(N_max / N_interval + 1).astype(np.int64), len(amplitudes)))
+    return Ns, np.array(sweep_values).reshape(np.round(N_max / N_interval + 1).astype(np.int64), len(amplitudes))
+
+
+def find_least_variation(x, ys, init_params=[1, 1], bounds=[[-100, -100],[100, 100]]):
+    # y_fits = []
+    # for y in ys:
+    #     par, y_fit = fit_function(x, y, linear_func, init_params, bounds)
+    #     y_fits.append(y_fit)
+    # y_fits = np.array(y_fits)
+    # diff = np.mean(np.abs(ys - y_fits[:, None]), axis=1)
+    diff = np.sum(ys, 1)
+    closest_idx = np.argmin(diff)
+    return closest_idx
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-pt", "--pulse_type", default="sq", type=str,
+    parser.add_argument("-pt", "--pulse_type", default="gauss", type=str,
         help="Pulse type (e.g. sq, gauss, sine, sech etc.)")
     parser.add_argument("-q", "--qubit", default=0, type=int,
         help="The number of the qubit to be used.")
-    parser.add_argument("-np", "--num_pulses", default=5, type=int,
-        help="Number of composite pulses to apply.")
-    parser.add_argument("-l", "--l", default=10, type=float,
-        help="Parameter l in Rabi oscillations fit")
-    parser.add_argument("-p", "--p", default=0.5, type=float,
-        help="Parameter p in Rabi oscillations fit")
+    parser.add_argument("-a", "--closest_amp", default=None, type=float,
+        help="First amp to use (should be close to PI area).")   
     parser.add_argument("-s", "--sigma", default=180, type=float,
         help="Pulse width (sigma) parameter")    
     parser.add_argument("-T", "--duration", default=2256, type=int,
-        help="Lorentz duration parameter")
-    parser.add_argument("-c", "--cutoff", default=0.5, type=float,
-        help="Cutoff parameter in PERCENT of maximum amplitude of Lorentzian")
+        help="Pulse duration parameter")
+    parser.add_argument("-N", "--N_max", default=100, type=int,
+        help="Maximum N number of pulses to use in the experiment.")
+    parser.add_argument("-Ni", "--N_interval", default=2, type=int,
+        help="Interval between N number of pulses.")
     parser.add_argument("-rb", "--remove_bg", default=1, type=int,
         help="Whether to drop the background (tail) of the pulse (0 or 1).")
-    parser.add_argument("-cp", "--control_param", default="width", type=str,
-        help="States whether width or duration is the controlled parameter")
     parser.add_argument("-epj", "--max_experiments_per_job", default=100, type=int,
         help="Maximum experiments per job")
     parser.add_argument("-ns", "--num_shots", default=2048, type=int,
@@ -99,187 +251,43 @@ if __name__ == "__main__":
     parser.add_argument("-b", "--backend", default="manila", type=str,
         help="The name of the backend to use in the experiment (one of perth, lagos, nairobi, \
         oslo, jakarta, manila, quito, belem, lima).")
-    parser.add_argument("-sp", "--span", default=0.005, type=float,
-        help="The span of the detuning sweep as a fraction of the driving frequency.")
+    parser.add_argument("-sp", "--span", default=0.01, type=float,
+        help="The span of the amplitude sweep as a fraction of the amplitude value.")
+    parser.add_argument("-iter", "--num_iterations", default=3, type=int,
+        help="The number of optimization iterations of the algorithm.")
     args = parser.parse_args()
 
     pulse_type = args.pulse_type
-    l = args.l
-    p = args.p
-    num_pulses = args.num_pulses
+    qubit = args.qubit
+    closest_amp = args.closest_amp
     sigma = args.sigma
-    duration = get_closest_multiple_of_16(args.duration)
-    cutoff = args.cutoff
+    duration = args.duration
+    N_max = args.N_max
+    N_interval = args.N_interval
     remove_bg = args.remove_bg
-    control_param = args.control_param
     max_experiments_per_job = args.max_experiments_per_job
     num_shots = args.num_shots
-    span = args.span
-    num_experiments = args.num_experiments
-    qubit = args.qubit
+    num_exp = args.num_experiments
     backend = args.backend
-    backend_name = backend
-    backend = "ibm_" + backend \
-        if backend in ["perth", "lagos", "nairobi", "oslo"] \
-            else "ibmq_" + backend
+    amp_span = args.span
+    num_iterations = args.num_iterations
 
-    ## create folder where plots are saved
-    file_dir = os.path.dirname(__file__)
-    file_dir = os.path.split(file_dir)[0]
-    date = datetime.now()
-    current_date = date.strftime("%Y-%m-%d")
-    calib_dir = os.path.join(file_dir, "calibrations")
-    save_dir = os.path.join(calib_dir, current_date)
-    # if not os.path.isdir(save_dir):
-    #     os.mkdir(save_dir)
-    data_folder = os.path.join(file_dir, "data", backend_name, "calibration", current_date)
-    # if not os.path.isdir(data_folder):
-    #     os.mkdir(data_folder)
-    make_all_dirs(save_dir)
-    make_all_dirs(data_folder)
-
-    # unit conversion factors -> all backend properties returned in SI (Hz, sec, etc)
-    GHz = 1.0e9 # Gigahertz
-    MHz = 1.0e6 # Megahertz
-    us = 1.0e-6 # Microseconds
-    ns = 1.0e-9 # Nanoseconds
-    mem_slot = 0
-
-    drive_chan = pulse.DriveChannel(qubit)
-    meas_chan = pulse.MeasureChannel(qubit)
-    acq_chan = pulse.AcquireChannel(qubit)
-
-    IBMQ.load_account()
-    provider = IBMQ.get_provider(hub='ibm-q')
-    backend = provider.get_backend(backend)
-    # backend = provider.get_backend("ibmq_qasm_simulator")
-    print(f"Using {backend_name} backend.")
-    backend_defaults = backend.defaults()
-    backend_config = backend.configuration()
-    center_frequency_Hz = backend_defaults.qubit_freq_est[qubit]
-    num_qubits = backend_config.n_qubits
-    if num_qubits <= qubit:
-        raise ValueError("Qubit index out of range for this system.")
-    span = span * center_frequency_Hz
-    frequencies = np.arange(center_frequency_Hz - span / 2, 
-                            center_frequency_Hz + span / 2,
-                            span / num_experiments)
-    print(f"The sweep will go from {np.round((center_frequency_Hz - span / 2) / GHz, 4)}"
-        f" GHz to {np.round((center_frequency_Hz + span / 2) / GHz, 4)} GHz"
-        f" in steps of {np.round((span / num_experiments) / MHz, 3)} MHz."
-        f" This results in {num_experiments} datapoints.")
-    dt = backend_config.dt
-    print(f"Sampling time: {dt*1e9} ns") 
-    amp = -np.log(1 - np.pi / l) / p
-    pulse_phases = pulse_sequence_dict[num_pulses]
-    freq = Parameter('freq')
-    with pulse.build(backend=backend, default_alignment='sequential', name="calibrate_freq") as sched:
-        dur_dt = duration
-        pulse.set_frequency(freq, drive_chan)
-
-        for n, p in enumerate(pulse_phases):
-            if pulse_type == "sq" or "sin" in pulse_type:
-                pulse_played = pulse_dict[pulse_type][remove_bg](
-                    duration=duration,
-                    amp=amp * np.exp(1j * p),
-                    name=pulse_type
-                )
-            elif pulse_type == "gauss":
-                pulse_played = pulse_dict[pulse_type][remove_bg](
-                    duration=duration,
-                    amp=amp * np.exp(1j * p),
-                    name=pulse_type,
-                    sigma=sigma / np.sqrt(2),
-                )
-            elif pulse_type in ["lor", "lor2", "lor3"]:
-                pulse_played = pulse_dict[pulse_type][remove_bg](
-                    duration=duration,
-                    amp=amp * np.exp(1j * p),
-                    name=pulse_type,
-                    sigma=sigma,
-                )
-            else:
-                pulse_played = pulse_dict[pulse_type][remove_bg](
-                    duration=duration,
-                    amp=amp * np.exp(1j * p),
-                    name=pulse_type,
-                    sigma=sigma,
-                )
-            # pulse.set_phase(p, drive_chan)
-            pulse.play(pulse_played, drive_chan)
-
-    # Create gate holder and append to base circuit
-    custom_gate = Gate("pi_pulse", 1, [freq])
-    base_circ = QuantumCircuit(num_qubits, 1)
-    base_circ.append(custom_gate, [qubit])
-    base_circ.measure(qubit, 0)
-    base_circ.add_calibration(custom_gate, (qubit,), sched, [freq])
-    circs = [base_circ.assign_parameters(
-            {freq: f},
-            inplace=False
-        ) for f in frequencies]
-
-    job_manager = IBMQJobManager()
-    job = job_manager.run(
-        circs,
-        backend=backend,
-        name="Frequency calibration",
-        max_experiments_per_job=max_experiments_per_job,
-        shots=num_shots
+    l, p, x0 = get_calib_params(
+        backend, pulse_type, 
+        sigma, duration,
+        remove_bg
     )
-    frequency_sweep_results = job.results()
-
-    sweep_values = []
-    for i in range(len(circs)):
-        counts = frequency_sweep_results.get_counts(i)["1"]
-        sweep_values.append(counts / num_shots)
-
-
-    frequencies_GHz = frequencies / GHz
-
-    data = {"frequency_ghz": frequencies_GHz, "transition_probability": (sweep_values)}
-    df = pd.DataFrame(data)
-    df.to_csv(
-        os.path.join(
-            data_folder, 
-            date.strftime("%H%M%S") + f"_calibration.csv"
-        ),
-        index=False
-    )
-    plt.figure(1)
-    plt.scatter(frequencies_GHz, (sweep_values), color='black') # plot real part of sweep values
-    plt.xlim([min(frequencies_GHz), max(frequencies_GHz)])
-    plt.title("Drive Frequency Calibration Curve")
-    plt.xlabel("Frequency [GHz]")
-    plt.ylabel("Transition Probability")
-    plt.savefig(os.path.join(save_dir, date.strftime("%H%M%S") + f"_frequency_sweep.png"))
-    # plt.show()
-
-    ## fit curve
-
-
-    def fit_function(x_values, y_values, function, init_params):
-        fitparams, conv = curve_fit(function, x_values, y_values, init_params)#, maxfev=100000)
-        y_fit = function(x_values, *fitparams)
-        
-        return fitparams, y_fit
-
-    fit_params, y_fit = fit_function(frequencies_GHz,
-                                    np.real(sweep_values), 
-                                    lambda x, A, q_freq, B, C: (A / np.pi) * (B / ((x - q_freq)**2 + B**2)) + C,
-                                    [0.3, 4.975, 0.2, 0] # initial parameters for curve_fit
-                                    )
-    plt.figure(2)
-    plt.scatter(frequencies_GHz, np.real(sweep_values), color='black')
-    plt.plot(frequencies_GHz, y_fit, color='red')
-    plt.xlim([min(frequencies_GHz), max(frequencies_GHz)])
-    plt.title("Fitted Drive Frequency Calibration Curve")
-    plt.xlabel("Frequency [GHz]")
-    plt.ylabel("Measured Signal [a.u.]")
-    plt.savefig(os.path.join(save_dir, date.strftime("%H%M%S") + f"_frequency_sweep_fitted.png"))
-    plt.show()
-
-    A, rough_qubit_frequency, B, C = fit_params
-    rough_qubit_frequency = rough_qubit_frequency*GHz # make sure qubit freq is in Hz
-    print(f"We've updated our qubit frequency estimate from "
-        f"{round(backend_defaults.qubit_freq_est[qubit] / GHz, 5)} GHz to {round(rough_qubit_frequency/GHz, 5)} GHz.")
+    for i in range(num_iterations):
+        x, ys = run_check(
+            amp_span / (10**i), 
+            duration, sigma, 
+            pulse_type, remove_bg,
+            num_exp=num_exp,
+            N_max=N_max, N_interval=N_interval,
+            max_exp_per_job=max_experiments_per_job,
+            num_shots=num_shots, 
+            backend=backend,
+            l=l, p=p, x0=x0,
+            closest_amp=closest_amp)
+        index = find_least_variation(x, ys)
+        closest_amp = x[index]
