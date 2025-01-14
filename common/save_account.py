2f8583edae5777ec6d05f1323419c1401459ca36
ivo <ivo.mihov@protonmail.com>
2025-01-14 21:41:33 +0200

    14/01 evening home

--- common/save_account.py
+++ common/save_account.py
@@ -1,5 +1,5 @@
 from qiskit_ibm_runtime import QiskitRuntimeService
 
-token = "6509a22fbce7376e8a2f48c952df448ad5bdc664061e55886887d68ae3c4283793eae37cf5a39ea36671e3f64f5d783b3f825a3444ff8604a2e9c09e31d68633"
+token = "50c94178c3193c95491c6d64675a0650f4e7457eea7f23b0799bd043d17c943a4368b24149cf8be172599a0b1ed6c0a610ef5a3672f9eea1f8116bb71f079ed0"
 
 QiskitRuntimeService.save_account(token, channel="ibm_quantum", instance='ibm-q/open/main', overwrite=True, set_as_default=True)
